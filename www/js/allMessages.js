80b817bc08093c0074368188c36c8a97fd3a7e92
danielmpla <danielmpla@gmail.com>
2013-11-27 15:48:43 +0100

    allMessages for test

---
+++ www/js/allMessages.js
@@ -0,0 +1,28 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Daniel
+ * Date: 27.11.13
+ * Time: 12:46
+ */
+$(document).ready(function () {
+    $.getJSON("http://141.46.136.3:8080/RisikousRESTful/rest/publications",
+        function (data) {
+            $.each(data, function (key, value) {
+                if (key == "publication") {
+                    $.each(value, function (publications) {
+                        var title;
+                        var date;
+                        $.each(publications, function (id, val) {
+                            if (id == "title") {
+                                title = val;
+                            }
+                            if (id == "entryDate") {
+                                date = val;
+                            }
+                        });
+                        $.append("<li>" + title + " " + date + "</li>", $('#list'));
+                    });
+                }
+            });
+        });
+});
