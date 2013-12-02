e544d92f2e037887eeecb70a35cd18a76b8e023b
danielmpla <danielmpla@gmail.com>
2013-12-02 10:34:29 +0100

    allMessages for test

--- www/js/allMessages.js
+++ www/js/allMessages.js
@@ -4,25 +4,35 @@
  * Date: 27.11.13
  * Time: 12:46
  */
-$(document).ready(function () {
-    $.getJSON("http://141.46.136.3:8080/RisikousRESTful/rest/publications",
-        function (data) {
-            $.each(data, function (key, value) {
-                if (key == "publication") {
-                    $.each(value, function (publications) {
-                        var title;
-                        var date;
-                        $.each(publications, function (id, val) {
-                            if (id == "title") {
-                                title = val;
-                            }
-                            if (id == "entryDate") {
-                                date = val;
-                            }
-                        });
-                        $.append("<li>" + title + " " + date + "</li>", $('#list'));
+$(document).ready(function() {
+
+    $.ajax({
+        headers: {
+            Accept : "application/json; charset=utf-8",
+            'Content-Type': 'application/json; charset=utf-8'
+
+        },
+        url : "http://141.46.136.3:8080/RisikousRESTful/rest/publications",
+        type : "GET",
+        dataType: "json",
+        crossDomain: true
+    }).success(function (data){
+        $.each(data, function(key, value){
+            if(key == "publication"){
+                $.each(value, function(publications){
+                    var title;
+                    var date;
+                    $.each(publications, function(id, val) {
+                       if(id == "title"){
+                           title = val;
+                       }
+                       if(id == "entryDate"){
+                           date = val;
+                       }
                     });
-                }
-            });
+                    $.append("<li>" + title + " " + date +  "</li>", $('#list'));
+                });
+            }
         });
+    });
 });
