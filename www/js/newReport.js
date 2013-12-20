ec460be9d4759509a1c256325570fc2738a723fd
danielmpla <danielmpla@gmail.com>
2013-12-20 09:07:14 +0100

    newReport for test

--- www/js/newReport.js
+++ www/js/newReport.js
@@ -2,10 +2,38 @@
  * Created by Daniel on 06.12.13.
  */
 $(document).ready(function () {
+    function simpleTextarea (text){
+        return '<textarea name="' + text +'" rows="6" placeholder="' + text + '"></textarea>';
+    }
+
     $.getJSON('http://141.46.136.3:8080/RisikousRESTful/rest/questionnaire', function (data) {
+        var options = {
+            allowFutureDates: false
+        };
+        var dateTime;
         $.each(data, function (key, value) {
-            if (key != "reportingArea") {
-                $('#report').append('<div id="' + key + '">', $('#report'));
+            if (key == "contactInformation") {
+                $('#report').append('<div id="' + key + '">' + simpleTextarea(data.contactInformation.text) + '</div>', $('#report'));
+            }
+            if (key == "immediateMeasure"){
+                $('#report').append('<div id="' + key + '">' + simpleTextarea(data.immediateMeasure.text) + '</div>', $('#report'));
+            }
+            if (key == "opinionOfReporter"){
+                $('#report').append('<div id="' + key + '">' + data.opinionOfReporter.text + '<br />' + simpleTextarea(data.opinionOfReporter.organisationalFactors.text) + '<br />' + simpleTextarea(data.opinionOfReporter.personalFactors.text) + '<br />' + simpleTextarea(data.opinionOfReporter.additionalNotes.text) + '</div>', $('#report'));
+            }
+            if (key == "incidentDescription"){
+                $('#report').append('<div id="' + key + '">' + simpleTextarea(data.incidentDescription.text) + '</div>', $('#report'));
+            }
+            if (key == "location"){
+                $('#report').append('<div id="' + key + '">' + simpleTextarea(data.location.text) + '</div>', $('#report'));
+            }
+            if (key == "consequences") {
+                $('#report').append('<div id="' + key + '">' + simpleTextarea(data.consequences.text) + '</div>', $('#report'));
+            }
+            if (key == "pointOfTime") {
+                $('#report').append('<div id="' + key + '">' + data.pointOfTime.text + '<br />' + datePicker.show(options, function (date) {
+                    dateTime = date;
+                }) + '</div>', $('#report'));
             }
             if (key == "reportingArea") {
                 var options;
